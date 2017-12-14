e8392cf70f4a570c4e60f292252b0c6d4ddfe251
amira hammad <amirahammad922@gmail.com>
2017-12-15 00:29:03 +0200

    update in registeration page

--- src/View/RegisterationPage.java
+++ src/View/RegisterationPage.java
@@ -1,93 +1,110 @@
 package View;
+  import javax.swing.*;
+
+import services.RegisterationServiceRequest;
 
 import java.awt.*;
+import java.awt.event.*;
 
- import javax.swing.*;
- 
 
-public class RegisterationPage {
-
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JPasswordField passwordField;
- 
-	public RegisterationPage() {
-		initialize();
-	}
-
+public class RegisterationPage extends JFrame implements ActionListener {
+	
+	 	private JFrame frame;
+	 	private JTextField  firstname;
+	 	private JTextField  lastname;
+	 	private JTextField  email;
+	 	private JPasswordField  password;
+	 	JButton  btn;
+	  
+	 	public RegisterationPage()   {
+	         initialize();
+	 	}
 	 
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1370, 546);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JLabel lblFirstname = new JLabel(" firstname :");
-		lblFirstname.setForeground(Color.ORANGE);
-		lblFirstname.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblFirstname.setBounds(10, 81, 99, 24);
-		frame.getContentPane().add(lblFirstname);
-		
-		textField = new JTextField();
-		textField.setBounds(119, 80, 235, 31);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel(" lastname :");
-		lblNewLabel.setForeground(Color.ORANGE);
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel.setBounds(10, 160, 87, 24);
-		frame.getContentPane().add(lblNewLabel);
-		
-		JLabel lblEmail = new JLabel("   email :");
-		lblEmail.setForeground(Color.ORANGE);
-		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblEmail.setBounds(10, 230, 87, 14);
-		frame.getContentPane().add(lblEmail);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(119, 224, 235, 31);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNewLabel_1 = new JLabel(" password :");
-		lblNewLabel_1.setForeground(Color.ORANGE);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_1.setBounds(10, 297, 99, 14);
-		frame.getContentPane().add(lblNewLabel_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(119, 158, 235, 33);
-		frame.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(119, 291, 235, 31);
-		frame.getContentPane().add(passwordField);
-		
-		JButton btnNewButton = new JButton(" Register");
-		btnNewButton.setForeground(Color.GRAY);
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 14));
-		btnNewButton.setBounds(119, 369, 99, 31);
-		frame.getContentPane().add(btnNewButton);
-		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\Freeware Sys\\Desktop\\spokesmodelSearch18-pastWinnersPanel-MichaelKatrina-desktop.jpg"));
-		lblNewLabel_2.setBounds(364, 0, 991, 514);
-		frame.getContentPane().add(lblNewLabel_2);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.DARK_GRAY);
-		panel.setBounds(0, 0, 362, 507);
-		frame.getContentPane().add(panel);
-		frame.setResizable( false);
-		frame.setVisible(true);
-		
-	}
+	 	 
+	 	private void initialize() {
+	 		frame = new JFrame();
+	 		frame.setBounds(100, 100, 1370, 546);
+	 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 		frame.getContentPane().setLayout(null);
+	 		
+	 		JLabel  fn = new JLabel(" firstname :");
+	 		 fn.setForeground(Color.ORANGE);
+	 		 fn.setFont(new Font("Tahoma", Font.BOLD, 14));
+	 	     fn.setBounds(10, 81, 99, 24);
+	 	    frame.getContentPane().add( fn);
+	 	
+	 		 firstname = new JTextField();
+	 	     firstname.setBounds(119, 80, 235, 31);
+			 frame.getContentPane().add( firstname);
+	 	     firstname.setColumns(10);
+	 	
+	 	  JLabel  ln = new JLabel(" lastname :");
+	 	     ln.setForeground(Color.ORANGE);
+	 		 ln.setFont(new Font("Tahoma", Font.BOLD, 14));
+	 		 ln.setBounds(10, 160, 87, 24);
+	 		frame.getContentPane().add( ln);
+	 		
+	 		JLabel  em = new JLabel("   email :");
+	 		 em.setForeground(Color.ORANGE);
+	 		 em.setFont(new Font("Tahoma", Font.BOLD, 14));
+	 		 em.setBounds(10, 230, 87, 14);
+	 		frame.getContentPane().add( em);
+	 		
+	 		 lastname = new JTextField();
+	 		 lastname.setBounds(119, 224, 235, 31);
+	 		frame.getContentPane().add( lastname);
+	 		 lastname.setColumns(10);
+	 		
+	 		JLabel  pass = new JLabel(" password :");
+	 		 pass.setForeground(Color.ORANGE);
+	 		 pass.setFont(new Font("Tahoma", Font.BOLD, 14));
+	 		 pass.setBounds(10, 297, 99, 14);
+	 		 frame.getContentPane().add( pass);
+	 		
+	 		 email = new JTextField();
+	 		 email.setBounds(119, 158, 235, 33);
+	 		 frame.getContentPane().add( email);
+	 		 email.setColumns(10);
+	 		
+	 		 password = new JPasswordField();
+	 		 password.setBounds(119, 291, 235, 31);
+	         frame.getContentPane().add( password);
+	 		
+	 	btn = new JButton(" Register");
+	 		 btn.setFont(new Font("Tahoma", Font.BOLD, 14));
+	 	     btn.setBounds(119, 369, 99, 31);
+	 		frame.getContentPane().add( btn);
+	 	
+	 		JLabel  label2 = new JLabel( );
+	 		 label2.setIcon(new ImageIcon("C:\\Users\\Freeware Sys\\Desktop\\spokesmodelSearch18-pastWinnersPanel-MichaelKatrina-desktop.jpg"));
+	 	      label2.setBounds(364, 0, 999, 514);
+	 		frame.getContentPane().add(label2);
+			
+	 		JPanel panel = new JPanel();
+	 		panel.setBackground(Color.DARK_GRAY);
+	 		panel.setBounds(0, 0, 362, 546);
+	 	    frame.getContentPane().add(panel);
+	 		frame.setResizable( false);
+	 		frame.setVisible(true);
+	 	}
 
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			  if(e.getSource()== btn)
+		        {
+		        JOptionPane.showMessageDialog(null, "Welcome Mr/Ms " +  firstname.getText());
+		           
+		        
+		        }
+			
+		}
+	 	 
 	 
-}
+	 	 
+	 }
+
+
+
 
 
